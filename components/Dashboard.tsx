8869e8985cb16c9f1aa9d08d824898d9533041ad
yccjs <yccjsalvador@gmail.com>
2022-05-25 17:26:04 +0900

    feat:responsive navbar

---
+++ components/Dashboard.tsx
@@ -0,0 +1,3 @@
+export default function Dashboard() {
+  return <div className="h-3/4 border border-solid w-1/3"></div>;
+}
---
+++ components/Navbar.tsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+
+export default function Navbar() {
+  const links: { name: string; link: string }[] = [
+    { name: "My Account", link: "/" },
+    { name: "Events", link: "/" },
+    { name: "Friends", link: "/" },
+  ];
+  let [open, setOpen] = useState(false);
+  return (
+    <nav className="shadow-md w-full fixed top-0 left-0">
+      <div className="md:flex items-center justify-between bg-white py-4 md:px-10 px-7">
+        <div className="font-bold text-2xl cursor-pointer flex items-center text-gray-800">
+          PlanOut
+        </div>
+        <div
+          onClick={() => setOpen(!open)}
+          className="text-3xl absolute right-8 top-6 cursor-pointer md:hidden"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth={2}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d={open ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16M4 18h16"}
+            />
+          </svg>
+        </div>
+        <ul
+          className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
+            open ? "top-20" : "top-[-490px]"
+          } `}
+        >
+          {links.map((link) => (
+            <li key={link.name} className="md:ml-8 text-xl md:my-0 my-7">
+              <a
+                href={link.link}
+                className="text-gray-800 hover:text-gray-400 duration-500"
+              >
+                {" "}
+                {link.name}{" "}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
+}
