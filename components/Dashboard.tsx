389474e4d33778b2c5783ffa51be9e11fbc86d2f
yccjs <yccjsalvador@gmail.com>
2022-05-27 10:38:36 +0900

    chore: try simple get request

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -1,3 +1,21 @@
-export default function Dashboard() {
+import { executionAsyncResource } from "async_hooks";
+import axios from "axios";
+import React, { useEffect } from "react";
+
+export default function Dashboard({ token }: string) {
+  useEffect(() => {
+    if (token) {
+      fetchData(token);
+    }
+  }, [token]);
+
+  const fetchData = async (token: string) => {
+    const response = await axios.get("http://localhost:8080/api", {
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    });
+    console.log(response);
+  };
   return <div className="h-3/4 border border-solid w-1/3"></div>;
 }
