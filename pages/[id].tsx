dd88595d309e8334fd1f345d4bb092cc0e6e2b85
Dominic Lowes <chaiskeng@gmail.com>
2022-05-30 18:55:09 +0900

    Refactor: change events map from div to link

---
+++ pages/[id].tsx
@@ -0,0 +1,9 @@
+const singleEvent = () => {
+    return (
+      <div>
+        <h1>Event page</h1>
+      </div>
+    );
+  }
+  
+  export default singleEvent;
--- pages/events.tsx
+++ pages/events.tsx
@@ -1,26 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import { withProtected } from "../src/hook/route";
+import Link from 'next/link';
 
 function Events() {
-  const events: { name: string }[] = [
-    { name: "Ruprecht's Birthday" },
-    { name: "Ruprecht's BBQ" },
-    { name: "Ruprecht's Bar Mitzvah" },
-  ];
+  interface Event {
+    id: number;
+    host: string;
+    name: string;
+    event_name: string;
+    date: Date;
+    budget: number;
+    created_at: number;
+    modified: number;
+  }
+  const [events, setEvents] = useState<Event[]>([]);
+
+  const showEvents = () => {
+    fetch("https://cc26-planout.herokuapp.com/events", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        // "x-auth-token": localStorage.getItem("token")
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setEvents(data);
+      });
+  };
+
+  useEffect(() => {
+    showEvents();
+  }, []);
+
   return (
     <div>
       <Navbar />
       <div className="container m-auto mt-20 box-content h-screen md:w-1/2 border-2">
         <div className="overflow-hidden m-10">
-          {" "}
           <div className="mt-10 text-center text-4xl font-header">EVENTS</div>
           <div>
             {events.map((event) => (
-              <div className="p-4 font-body text-2xl border-2 md:w-1/2 m-auto mt-10 text-center hover:cursor-pointer hover:border-blue-500 hover:bg-blue-100 transition-all duration-500 ease-in">
-                {" "}
-                {event.name}{" "}
-              </div>
+              <Link
+              className="p-4 font-body text-2xl border-2 md:w-1/2 m-auto mt-10 text-center hover:cursor-pointer hover:border-blue-500 hover:bg-blue-100 transition-all duration-500 ease-in"
+              href={"/event/" + event.id}
+                key={event.id}
+              >
+                {event.name}
+              </Link>
             ))}
           </div>
         </div>
