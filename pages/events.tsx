17d406359211df8492de27c9878f618db3d4fac6
Jon <25947147+JonrGull@users.noreply.github.com>
2022-05-30 11:17:38 +0900

    FEAT: Frontend display events from db

--- pages/events.tsx
+++ pages/events.tsx
@@ -1,23 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import { withProtected } from "../src/hook/route";
 
 function Events() {
-  const events: { name: string }[] = [
-    { name: "Ruprecht's Birthday" },
-    { name: "Ruprecht's BBQ" },
-    { name: "Ruprecht's Bar Mitzvah" },
-  ];
+  const [events, setEvents] = useState<any[]>([]);
+
+  const showEvents = () => {
+    fetch("http://localhost:8080/events", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        // "x-auth-token": localStorage.getItem("token")
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setEvents(data);
+      });
+  };
+
+  console.log(events);
+
+  useEffect(() => {
+    showEvents();
+  }, []);
+
   return (
     <div>
       <Navbar />
       <div className="container m-auto mt-20 box-content h-screen md:w-1/2 border-2">
         <div className="overflow-hidden m-10">
-          {" "}
           <div className="mt-10 text-center text-4xl font-header">EVENTS</div>
           <div>
             {events.map((event) => (
-              <div className="p-4 font-body text-2xl border-2 md:w-1/2 m-auto mt-10 text-center hover:cursor-pointer hover:border-blue-500 hover:bg-blue-100 transition-all duration-500 ease-in">
+              <div
+                key={event.id}
+                className="p-4 font-body text-2xl border-2 md:w-1/2 m-auto mt-10 text-center hover:cursor-pointer hover:border-blue-500 hover:bg-blue-100 transition-all duration-500 ease-in"
+              >
                 {event.name}
               </div>
             ))}
