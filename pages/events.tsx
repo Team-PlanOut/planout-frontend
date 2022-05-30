ad7fb15f6412d27caab9f174903617a6bc9eafb3
Jon <25947147+JonrGull@users.noreply.github.com>
2022-05-30 10:44:54 +0900

    DEL: home.module

--- pages/events.tsx
+++ pages/events.tsx
@@ -18,8 +18,7 @@ function Events() {
           <div>
             {events.map((event) => (
               <div className="p-4 font-body text-2xl border-2 md:w-1/2 m-auto mt-10 text-center hover:cursor-pointer hover:border-blue-500 hover:bg-blue-100 transition-all duration-500 ease-in">
-                {" "}
-                {event.name}{" "}
+                {event.name}
               </div>
             ))}
           </div>
