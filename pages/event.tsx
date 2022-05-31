2ed4b2ff86bf657d3bd496b4f2f26f47e0f00f69
Konstantin Shambazov <k.shambazov@gmail.com>
2022-05-31 15:45:12 +0900

    feat: add display all tasks per event functionality

--- pages/event.tsx
+++ pages/event.tsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import { withProtected } from "../src/hook/route";
 import { useRouter } from "next/router";
+import Link from "next/link";
+
 
 function singleEvent() {
     const router = useRouter()
@@ -19,7 +21,10 @@ function singleEvent() {
         created_at: number;
         modified: number;
       }
+
+
       const [events, setEvents] = useState<Event[]>([]);
+      const [tasks, setTasks] = useState([])
     
       const showEvents = async() => {
        await fetch(`https://cc26-planout.herokuapp.com/events/${router.query.id}`, {
@@ -34,10 +39,31 @@ function singleEvent() {
             setEvents(data);
           });
       };
+
+      const showTasks = async() => {
+        await fetch(`https://cc26-planout.herokuapp.com/tasks/event/${router.query.id}`, {
+           method: "GET",
+           headers: {
+             "Content-Type": "application/json",
+             // "x-auth-token": localStorage.getItem("token")
+           },
+         })
+           .then((res) => res.json())
+           .then((data) => {
+             setTasks(data);
+           });
+       };
+ 
+
     
       useEffect(() => {
         showEvents();
       }, [events]);
+
+      useEffect(()=>{
+        showTasks();
+      },[tasks]);
+
     return (
         <div>
             <Navbar/>
@@ -45,7 +71,20 @@ function singleEvent() {
         <div className="overflow-hidden m-10">
           <div className="mt-10 text-center text-4xl font-header">{events.name}</div>
           <div>TASKS</div>
+        <div className="overflow-hidden m-10">
+          <div>
+            {tasks.map((task) => (
+              <div className="p-4 font-body text-2xl border-2 md:w-1/2 m-auto mt-10 text-center hover:cursor-pointer hover:border-blue-500 hover:bg-blue-100 transition-all duration-500 ease-in">
+                <div
+                  key={task.id}
+                >
+                  {task.description}
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
+      </div>
       </div>
         </div>
     )
