f06bb00eb606213a6987b4452bc44c4d2939c65c
yccjs <yccjsalvador@gmail.com>
2022-05-31 13:13:23 +0900

    feat: add username and pic

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -3,28 +3,36 @@ import React, { useEffect } from "react";
 import useAuth from "../src/hook/auth";
 
 export default function Dashboard() {
-  const { token } = useAuth() as any;
-
+  const { token, user } = useAuth() as any;
+  console.log(user);
   useEffect(() => {
     if (token) {
       fetchData(token);
     }
-  }, [token]);
+  }, []);
 
   const fetchData = async (token: string) => {
-    const response = await axios.get("http://localhost:8080/", {
-      headers: {
-        Authorization: "Bearer " + token,
-      },
-    });
+    const response = await axios.get(
+      "https://cc26-planout.herokuapp.com/events",
+      {
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      }
+    );
     console.log("this is response", response);
   };
+
   return (
     <div className="container m-auto mt-20 box-content h-screen md:w-1/2 border">
       <div className="overflow-hidden m-10">
         {" "}
-        <div className="bg-blue-200 rounded-full w-40 h-40 m-auto"></div>
-        <div className="text-center mt-5">user's name here</div>
+        <div>
+          <img src={user.photoURL} className=" m-auto"></img>
+        </div>
+        <div className="text-center mt-5 font-body text-2xl">
+          {user.displayName}
+        </div>
         <div className="mt-10 text-center text-4xl font-header">
           {" "}
           Current Tasks
