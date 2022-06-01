d739ea18e7b92a68358496a0e1f4867839fe418d
yccjs <yccjsalvador@gmail.com>
2022-06-01 19:57:54 +0900

    chore: edit modal's close icon's position

--- components/EventModal.tsx
+++ components/EventModal.tsx
@@ -25,7 +25,7 @@ export default function Event({
         <div className="m-auto bg-white e rounded-lg shadow dark:bg-gray-700 ">
           <button
             type="button"
-            className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
+            className=" float-right mr-2 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
             data-modal-toggle="authentication-modal"
             onClick={() => setShowModal(false)}
           >
