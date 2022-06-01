1691f0788ea27b1bd41d809512156ee7f5247007
yccjs <yccjsalvador@gmail.com>
2022-06-01 20:00:15 +0900

    chore: edit task modal's close icon's position and center modal for task

--- components/TaskModal.tsx
+++ components/TaskModal.tsx
@@ -19,11 +19,11 @@ export default function Task({
       aria-hidden="true"
       className="auto overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full"
     >
-      <div className="relative p-4 w-full max-w-md h-full md:h-auto">
-        <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+      <div className="m-auto mt-16 p-4 w-full max-w-md h-full md:h-auto">
+        <div className="m-auto bg-white rounded-lg shadow dark:bg-gray-700">
           <button
             type="button"
-            className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
+            className="float-right mr-2 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
             data-modal-toggle="authentication-modal"
             onClick={() => setShowModal(false)}
           >
