b74e08093bb1b6402145de81e7c911ff0bbfee47
juliwithoutthee <jnboucree@gmail.com>
2022-06-06 18:03:47 +0900

    feat: add stripe price id to access stripe api

--- pages/api/checkout_sessions.ts
+++ pages/api/checkout_sessions.ts
@@ -8,7 +8,7 @@ export default async function handler(req: any, res: any) {
         line_items: [
           {
             // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-            price: '{{PRICE_ID}}',
+            price: 'price_1L7avLF9KFH9wctxS20KIO6S',
             quantity: 1,
           },
         ],
