6d95cfef5e475181342d43e9dd59297afcca6e04
Dominic Lowes <chaiskeng@gmail.com>
2022-06-06 14:13:28 +0900

    Feat: add function to get friends

--- pages/friends.tsx
+++ pages/friends.tsx
@@ -1,17 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { FaPersonBooth } from "react-icons/fa";
+import axios from "axios";
 
 import Navbar from "../components/Navbar";
 import { withProtected } from "../src/hook/route";
+import useAuth from "./../src/hook/auth";
 
 function Friends() {
+  const [friends, setFriends] = useState<Friend[]>([]);
+  const { token } = useAuth() as any;
+
+  const getFriends = async() => {
+    // need a way to access logged in id
+    const response = await axios.get(
+      `https://cc26-planout.herokuapp.com/friends/${id}`,
+      {
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      }
+    );
+    setFriends(response.data);
+};
+
+useEffect(() => {
+  getFriends()
+},[]);
+
+
   return (
     <div>
       <Navbar />
       <div className="container m-auto h-auto mt-20 box-content md:w-1/2 shadow md:shadow-lg pb-10">
         <div>Friend List</div>
         <div>
-          <FaPersonBooth />
+          <div>
+            {friends.map((friend) => (
+              <div key={friend.id}>
+                {friend.firstName}
+                </div>
+            ))}
+          </div>
+          {/* <FaPersonBooth /> */}
         </div>
       </div>
     </div>
