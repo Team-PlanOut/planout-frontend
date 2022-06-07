b06dc638fe10bc8e16a5c6af512e5f6b80514551
yccjs <yccjsalvador@gmail.com>
2022-06-07 19:07:45 +0900

    chore: change pay button style

--- components/StripeCheckout.tsx
+++ components/StripeCheckout.tsx
@@ -1,59 +1,38 @@
-import React from 'react'
+import React from "react";
 
-import { loadStripe } from '@stripe/stripe-js';
+import { loadStripe } from "@stripe/stripe-js";
 
-const stripePromise = loadStripe( "pk_test_51L7UpKF9KFH9wctxGfqKckcUDmzhVUEkGZQTCeXZjSYvI5k33s1oXe3rIPJP5I8Un5qV5q016WE0aW4NcI3wThOx00OFftpTkL" //this is hard coded
-  //process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY 
+const stripePromise = loadStripe(
+  "pk_test_51L7UpKF9KFH9wctxGfqKckcUDmzhVUEkGZQTCeXZjSYvI5k33s1oXe3rIPJP5I8Un5qV5q016WE0aW4NcI3wThOx00OFftpTkL" //this is hard coded
+  //process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
 );
 
 export default function StripeCheckout() {
-    React.useEffect(() => {
-      // Check to see if this is a redirect back from Checkout
-      const query = new URLSearchParams(window.location.search);
-      if (query.get('success')) {
-        console.log('Order placed! You will receive an email confirmation.');
-      }
-  
-      if (query.get('canceled')) {
-        console.log('Order canceled -- continue to shop around and checkout when you’re ready.');
-      }
-    }, []);
-  
-    return (
-      <form action="/api/checkout_sessions" method="POST">
-        <section>
-            
-          <button type="submit" role="link">
-            Checkout
-          </button>
-        </section>
-        <style jsx>
-          {`
-            section {
-              background: #ffffff;
-              display: flex;
-              flex-direction: column;
-              width: 16.5em;
-              height: 10;
-              border-radius: 6px;
-              justify-content: space-between;
-            }
-            button {
-              height: 36px;
-              background: #556cd6;
-              border-radius: 4px;
-              color: white;
-              border: 0;
-              font-weight: 600;
-              cursor: pointer;
-              transition: all 0.2s ease;
-              box-shadow: 0px 4px 5.5px 0px rgba(0, 0, 0, 0.07);
-            }
-            button:hover {
-              opacity: 0.8;
-            }
-          `}
-        </style>
-      </form>
-    );
-  }
+  React.useEffect(() => {
+    // Check to see if this is a redirect back from Checkout
+    const query = new URLSearchParams(window.location.search);
+    if (query.get("success")) {
+      console.log("Order placed! You will receive an email confirmation.");
+    }
+
+    if (query.get("canceled")) {
+      console.log(
+        "Order canceled -- continue to shop around and checkout when you’re ready."
+      );
+    }
+  }, []);
+
+  return (
+    <form action="/api/checkout_sessions" method="POST">
+      <section>
+        <button
+          type="submit"
+          role="link"
+          className="bg-orange-300 font-medium inline-flex items-center px-3 py-1 rounded-md shadow-md text-white transition hover:bg-orange-400"
+        >
+          Pay
+        </button>
+      </section>
+    </form>
+  );
+}
