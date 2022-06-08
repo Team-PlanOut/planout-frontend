fe444d82d7fa68cb8648c9c8c8c2a4de82e7f930
yccjs <yccjsalvador@gmail.com>
2022-06-08 11:14:34 +0900

    chore: clean up code

--- pages/friends.tsx
+++ pages/friends.tsx
@@ -12,7 +12,7 @@ function Friends() {
   const [friends, setFriends] = useState<Friends[]>([]);
   const { token } = useAuth() as any;
   const { user } = useAuth() as any;
-  const [addFriend, setAddFriend] = useState(false);
+  const [addFriend, setAddFriend] = useState<boolean>(false);
 
   const getFriends = async () => {
     const id = user.uid;
@@ -49,7 +49,7 @@ function Friends() {
           Add Friend
         </button>
         {addFriend ? (
-          <AddFriend setAddFriend={setAddFriend} />
+          <AddFriend />
         ) : (
           <div>
             <div>
