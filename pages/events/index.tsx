9191566efb1185bbe9ccc9236896c56e5c34cab8
yccjs <yccjsalvador@gmail.com>
2022-06-09 16:55:36 +0900

    chore: build fail fix attempt

--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -48,36 +48,34 @@ function Events() {
             <EventForm getEvents={getEvents} />
           </div>
           {events.map((event) => (
-            <div>
-              <div
+            <div
+              key={event.id}
+              className="pb-8 pt-4 pr-2 font-body rounded-md text-2xl mb-2 shadow-md md:w-1/2 m-auto mt-10 text-center hover:border-blue-500 hover:bg-blue-50 transition-all duration-500 ease-in bg-gray-50"
+            >
+              <FaTrash
+                onClick={() => {
+                  deleteEvent(event.id);
+                  getEvents();
+                }}
+                className="text-sm float-right md:fill-gray-50 hover:cursor-pointer hover:fill-black"
+              />
+
+              <Link
+                href="/events/[id]"
+                as={`/events/${event.id}`}
                 key={event.id}
-                className="pb-8 pt-4 pr-2 font-body rounded-md text-2xl mb-2 shadow-md md:w-1/2 m-auto mt-10 text-center hover:border-blue-500 hover:bg-blue-50 transition-all duration-500 ease-in bg-gray-50"
               >
-                <FaTrash
-                  onClick={() => {
-                    deleteEvent(event.id);
-                    getEvents();
-                  }}
-                  className="text-sm float-right md:fill-gray-50 hover:cursor-pointer hover:fill-black"
-                />
-
-                <Link
-                  href="/events/[id]"
-                  as={`/events/${event.id}`}
-                  key={event.id}
-                >
-                  <div className="hover:cursor-pointer">
-                    <div className="text-center capitalize ">{event.name}</div>
-                    <div className="text-center mt-2">
-                      {showOnlyDate(event.date)}
-                    </div>
+                <div className="hover:cursor-pointer">
+                  <div className="text-center capitalize ">{event.name}</div>
+                  <div className="text-center mt-2">
+                    {showOnlyDate(event.date)}
                   </div>
-                </Link>
-
-                <div className="text-sm float-right">
-                  {" "}
-                  Hosted by {event.hostFirstName} {event.hostLastName}{" "}
                 </div>
+              </Link>
+
+              <div className="text-sm float-right">
+                {" "}
+                Hosted by {event.hostFirstName} {event.hostLastName}{" "}
               </div>
             </div>
           ))}
