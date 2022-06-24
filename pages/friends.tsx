4e8e536ae5c9356230f8fc2b52582569e82db802
yccjs <yccjsalvador@gmail.com>
2022-06-24 13:27:06 +0900

    feat: new colors for friends

--- pages/friends.tsx
+++ pages/friends.tsx
@@ -39,45 +39,47 @@ function Friends() {
   return (
     <div>
       <Navbar />
-      <div className="shadow-2xl container m-auto h-screen md:w-1/2  overflow-y-auto mt-20 box-content bg-container bg-opacity-10 pb-10">
-        <div className="text-center text-4xl mt-4 font-body font-bold">
+      <div className="h-screen">
+        <div className="text-center text-4xl font-body font-bold mt-24">
           Friends List
         </div>
-        <button
-          onClick={addUserAsFriend}
-          className="bg-buttonColor ml-8 mt-4 font-body  text-xl font-medium m-auto items-center px-3 py-1 rounded-md shadow-md text-white transition hover:bg-blue-300"
-        >
-          Add Friend
-        </button>
-        {addFriend ? (
-          <AddFriend
-            setAddFriend={setAddFriend}
-            getFriends={getFriends}
-            friends={friends}
-          />
-        ) : (
-          <div className=" flex flex-col justify-center items-center">
-            {friends.map((friend) => (
-              <div
-                key={friend.friendId}
-                className="flex flex-row w-2/3 rounded-lg font-body text-xl border-2 p-4 bg-container bg-opacity-20  md:w-80 mt-10 text-center hover:cursor-pointer hover:border-white  transition-all duration-500 ease-in"
-              >
-                <div>
-                  <img
-                    src={friend.photoUrl}
-                    className="rounded-full w-16 h-16"
-                  />
-                </div>
-                <div className="ml-4 flex flex-col justify-center">
-                  <div key={friend.friendId} className="font-semibold">
-                    {friend.friendFirstName} {friend.friendLastName}
+        <div className="shadow-2xl container m-auto h-4/8 md:w-1/2 scrollbar overflow-y-auto mt-10 rounded-lg box-content bg-dashboard pb-10">
+          <button
+            onClick={addUserAsFriend}
+            className="bg-buttonColor scale-100 hover:scale-110 ml-8 mt-4 font-body  text-xl font-medium m-auto items-center px-3 py-1 rounded-md shadow-md text-white transition hover:bg-blue-300"
+          >
+            Add Friend
+          </button>
+          {addFriend ? (
+            <AddFriend
+              setAddFriend={setAddFriend}
+              getFriends={getFriends}
+              friends={friends}
+            />
+          ) : (
+            <div className=" flex flex-col justify-center items-center">
+              {friends.map((friend) => (
+                <div
+                  key={friend.friendId}
+                  className="flex flex-row w-2/3 rounded-lg font-body text-xl border-2 p-4 bg-yellow-100 scale-100 hover:scale-110  md:w-80 mt-10 text-center hover:cursor-pointer hover:border-white  transition-all duration-500 ease-in"
+                >
+                  <div>
+                    <img
+                      src={friend.photoUrl}
+                      className="rounded-full w-16 h-16"
+                    />
                   </div>
-                  <div className="text-sm"> {friend.email}</div>
+                  <div className="ml-4 flex flex-col justify-center">
+                    <div key={friend.friendId} className="font-semibold">
+                      {friend.friendFirstName} {friend.friendLastName}
+                    </div>
+                    <div className="text-sm"> {friend.email}</div>
+                  </div>
                 </div>
-              </div>
-            ))}
-          </div>
-        )}
+              ))}
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
